f84ea848c1e2723f91b23f2b46f93e27e0302ce2
Nico FRANCOIS <nfrancois45@gmail.com>
2018-11-26 23:05:00 +0100

    first Commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,17 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
+require('../css/app.scss');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // var $ = require('jquery');
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
+const $ = require('jquery');
+
+require('bootstrap');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
