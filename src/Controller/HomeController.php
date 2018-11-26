5f7bcb8d7efcb4990f86728445a928d6b7275db1
Nico FRANCOIS <nfrancois45@gmail.com>
2018-11-27 00:20:00 +0100

    navbar skeleton create, link userpage, btn admin

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * Class homeController
+ * @package App\Controller
+ */
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/home", name="home")
+     */
+    public function index()
+    {
+        return $this->render('home/index.html.twig');
+    }
+}
