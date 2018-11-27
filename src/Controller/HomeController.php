d9ac6ecbe9e0b4ba7fadf6e3bfee4d487b8a2c4f
Florence LG <flo.lg.gautier@gmail.com>
2018-11-27 16:32:39 +0100

    ajout du package app\controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class HomeController
+ * @package App\Controller
+ */
 class HomeController extends AbstractController
 {
     /**
