a5ff2b007a08ba0b68014785975a3dc81056d4d0
Nico FRANCOIS <nfrancois45@gmail.com>
2018-11-27 16:30:53 +0100

    logo_integration/fin US

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,12 +11,16 @@ require('../css/app.scss');
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // var $ = require('jquery');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
-
 const $ = require('jquery');
 
 require('bootstrap');
+import "bootstrap";
 
 $(document).ready(function() {
     $('[data-toggle="popover"]').popover();
 });
+//
+// $('#myTabs a').click(function (e) {
+//     e.preventDefault()
+//     $(this).tab('show')
+// })
