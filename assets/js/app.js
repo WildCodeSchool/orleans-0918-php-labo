4603714ba5517316364ee47cf7b95ae22eab93b8
Helene Bloch <ln-45@live.fr>
2018-11-27 16:24:39 +0100

    retrait du console.log dans le fichier app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,5 +11,4 @@ require('../css/app.scss');
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // var $ = require('jquery');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
 import "bootstrap";
