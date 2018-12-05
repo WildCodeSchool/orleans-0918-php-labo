e9cf3ba48b978f411ac1be8040cfcb2745a1d367
Helene Bloch <ln-45@live.fr>
2018-12-05 15:32:27 +0100

    retrait du use Staff Entity

--- src/Form/ReservationType.php
+++ src/Form/ReservationType.php
@@ -4,7 +4,6 @@ namespace App\Form;
 
 use App\Entity\Reservation;
 use App\Entity\Room;
-use App\Entity\Staff;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
