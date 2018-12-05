b7ea9df9029640002abe7ecb68f9d582089ba01b
Florence LG <flo.lg.gautier@gmail.com>
2018-12-05 10:29:30 +0100

    remis le forme en place, remplacement de save en valider et assert\regex mis en place

--- src/Entity/Staff.php
+++ src/Entity/Staff.php
@@ -5,6 +5,8 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\StaffRepository")
@@ -20,11 +22,13 @@ class Staff
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Regex("/^[a-zA-Z]+$/", message="Vous ne pouvez pas mettre de chiffres")
      */
     private $lastname;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Regex("/^[a-zA-Z]+$/", message="Vous ne pouvez pas mettre de chiffres")
      */
     private $firstname;
 
