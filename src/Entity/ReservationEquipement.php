ee6f43f7500d4d8258ae653b6867be2f3e45157c
Thierry STOLLSTEINER <stollsteiner.thierry@hotmail.fr>
2018-12-13 09:58:09 +0100

    ajout des Assert sur la propriété RéservationEquipement

--- src/Entity/ReservationEquipement.php
+++ src/Entity/ReservationEquipement.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ReservationEquipementRepository")
@@ -18,6 +19,8 @@ class ReservationEquipement
 
     /**
      * @ORM\Column(type="integer", nullable=true)
+     * @Assert\GreaterThanOrEqual(0)
+     * @Assert\Type("integer")
      */
     private $quantity;
 
