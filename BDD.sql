432c95cec073013a7a1276ac4fd68e12ab4379ec
Thierry STOLLSTEINER <stollsteiner.thierry@hotmail.fr>
2019-01-14 11:58:36 +0100

    modification de BDD.sql pour ne mettre en SQL que le contenu des tables et non de la base complète

--- BDD.sql
+++ BDD.sql
@@ -16,69 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `company`
---
-
-DROP TABLE IF EXISTS `company`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `company` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `company_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `company`
---
-
-LOCK TABLES `company` WRITE;
-/*!40000 ALTER TABLE `company` DISABLE KEYS */;
-/*!40000 ALTER TABLE `company` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `customer`
---
-
-DROP TABLE IF EXISTS `customer`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `customer` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `last_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `first_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `phone_number` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `mail_address` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `customer`
---
-
-LOCK TABLES `customer` WRITE;
-/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
-/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `equipement`
---
-
-DROP TABLE IF EXISTS `equipement`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `equipement` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Dumping data for table `equipement`
 --
 
@@ -89,161 +26,14 @@ INSERT INTO `equipement` VALUES (1,'Clef'),(2,'Cable HDMI'),(3,'Badge'),(4,'Adap
 UNLOCK TABLES;
 
 --
--- Table structure for table `migration_versions`
---
-
-DROP TABLE IF EXISTS `migration_versions`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `migration_versions` (
-  `version` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`version`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `migration_versions`
---
-
-LOCK TABLES `migration_versions` WRITE;
-/*!40000 ALTER TABLE `migration_versions` DISABLE KEYS */;
-INSERT INTO `migration_versions` VALUES ('20190110165825');
-/*!40000 ALTER TABLE `migration_versions` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `reservation`
---
-
-DROP TABLE IF EXISTS `reservation`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `reservation` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `company_id` int(11) NOT NULL,
-  `customer_id` int(11) NOT NULL,
-  `staff_id` int(11) NOT NULL,
-  `comment` longtext COLLATE utf8mb4_unicode_ci,
-  `signature` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `start_date` datetime NOT NULL,
-  `is_archived` tinyint(1) NOT NULL,
-  `end_date` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `IDX_42C84955979B1AD6` (`company_id`),
-  KEY `IDX_42C849559395C3F3` (`customer_id`),
-  KEY `IDX_42C84955D4D57CD` (`staff_id`),
-  CONSTRAINT `FK_42C849559395C3F3` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`),
-  CONSTRAINT `FK_42C84955979B1AD6` FOREIGN KEY (`company_id`) REFERENCES `company` (`id`),
-  CONSTRAINT `FK_42C84955D4D57CD` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `reservation`
---
-
-LOCK TABLES `reservation` WRITE;
-/*!40000 ALTER TABLE `reservation` DISABLE KEYS */;
-/*!40000 ALTER TABLE `reservation` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `reservation_equipement`
---
-
-DROP TABLE IF EXISTS `reservation_equipement`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `reservation_equipement` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `reservation_id` int(11) NOT NULL,
-  `equipement_id` int(11) NOT NULL,
-  `quantity` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `equipementreservation_unique` (`reservation_id`,`equipement_id`),
-  KEY `IDX_D81E0F47B83297E7` (`reservation_id`),
-  KEY `IDX_D81E0F47806F0F5C` (`equipement_id`),
-  CONSTRAINT `FK_D81E0F47806F0F5C` FOREIGN KEY (`equipement_id`) REFERENCES `equipement` (`id`),
-  CONSTRAINT `FK_D81E0F47B83297E7` FOREIGN KEY (`reservation_id`) REFERENCES `reservation` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `reservation_equipement`
---
-
-LOCK TABLES `reservation_equipement` WRITE;
-/*!40000 ALTER TABLE `reservation_equipement` DISABLE KEYS */;
-/*!40000 ALTER TABLE `reservation_equipement` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `reservation_room`
---
-
-DROP TABLE IF EXISTS `reservation_room`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `reservation_room` (
-  `reservation_id` int(11) NOT NULL,
-  `room_id` int(11) NOT NULL,
-  PRIMARY KEY (`reservation_id`,`room_id`),
-  KEY `IDX_64A69CF3B83297E7` (`reservation_id`),
-  KEY `IDX_64A69CF354177093` (`room_id`),
-  CONSTRAINT `FK_64A69CF354177093` FOREIGN KEY (`room_id`) REFERENCES `room` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `FK_64A69CF3B83297E7` FOREIGN KEY (`reservation_id`) REFERENCES `reservation` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `reservation_room`
---
-
-LOCK TABLES `reservation_room` WRITE;
-/*!40000 ALTER TABLE `reservation_room` DISABLE KEYS */;
-/*!40000 ALTER TABLE `reservation_room` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `room`
---
-
-DROP TABLE IF EXISTS `room`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `room` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `door` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Dumping data for table `room`
 --
 
 LOCK TABLES `room` WRITE;
 /*!40000 ALTER TABLE `room` DISABLE KEYS */;
-INSERT INTO `room` VALUES (1,'Showroom',1),(2,'Délirium',1),(3,'Fabrique',1),(4,'Hub 1',1),(5,'Hub 2',1),(6,'Shaker',1),(7,'Imaginarium',1),(8,'Tipi',1),(9,'Bulle 1',2),(10,'Bulle 2',2),(11,'Bootcamp',1),(12,'Club',2),(13,'Ruche',1),(14,'Tremplin',1);
+INSERT INTO `room` VALUES (1,'Showroom',1),(2,'Délirium',1),(3,'Fabrique',1),(4,'Hub 1',1),(5,'Hub 2',1),(7,'Imaginarium',1),(8,'Tipi',1),(10,'Bulle 2',2),(11,'Bootcamp',1),(12,'Club',2),(13,'Ruche',1),(14,'Tremplin',1);
 /*!40000 ALTER TABLE `room` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `staff`
---
-
-DROP TABLE IF EXISTS `staff`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `staff` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `lastname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `firstname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `is_active` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `staff`
@@ -251,7 +41,7 @@ CREATE TABLE `staff` (
 
 LOCK TABLES `staff` WRITE;
 /*!40000 ALTER TABLE `staff` DISABLE KEYS */;
-INSERT INTO `staff` VALUES (1,'VAILLANT','Julie',1),(2,'BOULAS','Pauline',1),(3,'COURTOIS','Ludovic',1),(4,'DROIN','Anthonie',1),(5,'PARFUM','Sylvie',1);
+INSERT INTO `staff` VALUES (1,'VAILLANT','Julie',0),(2,'BOULAS','Pauline',1),(3,'COURTOIS','Ludovic',1),(4,'DROIN','Anthonie',1),(5,'PARFUM','Sylvie',1);
 /*!40000 ALTER TABLE `staff` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -264,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-01-10 18:08:04
+-- Dump completed on 2019-01-14 11:56:12
